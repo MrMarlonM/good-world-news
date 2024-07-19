15ae4acb9b7a2c995b1779da2924a2988afcdb45
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-07-19 14:05:48 +0000

    Add article model

--- newsfeed/models.py
+++ newsfeed/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
