b05cc5b51ea727a6b12e4b64cb3bba3cf2cd9a71
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-07-22 09:34:04 +0000

    Add allowed host herokuapp

--- good_world_news/settings.py
+++ good_world_news/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['8000-mrmarlonm-goodworldnews-vt95vfo77o6.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-mrmarlonm-goodworldnews-vt95vfo77o6.ws.codeinstitute-ide.net',
+                '.herokuapp.com']
 
 
 # Application definition
