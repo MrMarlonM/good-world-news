d2026ea9ea7b2762aa8d4a3ec3ae725821917a35
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-07-22 08:54:47 +0000

    Update Article model

--- newsfeed/models.py
+++ newsfeed/models.py
@@ -10,6 +10,7 @@ class Article(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    excerpt = models.TextField(blank=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
