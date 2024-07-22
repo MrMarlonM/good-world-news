8548f3459e375a00a0ba299ba18cc8f6c953c36e
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-07-22 07:36:11 +0000

    Add comment and like model to newsfeed app

--- newsfeed/models.py
+++ newsfeed/models.py
@@ -11,3 +11,17 @@ class Article(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    content_type = models.ForeignKey(ContentType, on_delte=models.CASCADE)
+    object_id = models.PositiveIntegerField()
