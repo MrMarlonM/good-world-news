3679f4040e4d5c708318e7de9755516455cd15aa
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-07-30 09:09:38 +0000

    Add views.py and urls.py to contact app, create basic contact.html and wire all up

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactForm
 
 # Create your views here.
+def contact_form(request):
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, 'Your messages was sent successfully.')
+            return redirect('home')
+
+    else:
+        form = ContactForm()
+    return render(request, 'contact/contact.html', {'form': form})
--- good_world_news/urls.py
+++ good_world_news/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path('summernote/', include('django_summernote.urls')),
+    path('contact/', include('contact.urls'), name='contact'),
     path('', include("newsfeed.urls"), name='newsfeed-urls'),
 ]
