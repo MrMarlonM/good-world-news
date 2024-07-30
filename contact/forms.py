1a8a478c3b7c62c3cd28f95dec5878683033ca10
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-07-30 09:31:35 +0000

    Update forms.py and contact.html to customize style

--- contact/forms.py
+++ contact/forms.py
@@ -6,6 +6,8 @@ class ContactForm(forms.ModelForm):
         model = Contact
         fields = ['name', 'email', 'reason', 'message']
         widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
             'reason': forms.Select(attrs={'class': 'form-select'}),
             'message': forms.Textarea(attrs={'class': 'form-control'})
         }
