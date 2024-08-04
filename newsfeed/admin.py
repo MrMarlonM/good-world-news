8799d4c80073553c7c1b60384e1ceb1ab62ace65
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-08-04 12:55:21 +0000

    Add docstrings to admin in newsfeed

--- newsfeed/admin.py
+++ newsfeed/admin.py
@@ -5,6 +5,9 @@ from .models import Article, Comment, Like
 
 @admin.register(Article)
 class ArticleAdmin(SummernoteModelAdmin):
+    """
+    Customizes the Django admin interface for the Article model.
+    """
     list_display = ('title', 'author', 'created_on', 'status',)
     list_filter = ('status', 'created_on', 'author')
     prepopulated_fields = {'slug': ('title',)}
@@ -13,8 +16,11 @@ class ArticleAdmin(SummernoteModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(SummernoteModelAdmin):
+    """
+    Customizes the Django admin interface for the Comment model.
+    """
     list_display = ('author', 'article', 'created_on', 'approved')
     list_filter = ('author', 'created_on', 'approved')
 
-# Register your models here.
+# Registration of Like model
 admin.site.register(Like)
