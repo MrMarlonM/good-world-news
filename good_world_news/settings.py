d2f790698819b4ecd512b170a0427d76602545c5
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-08-04 13:10:42 +0000

    Delete unused database from settings

--- good_world_news/settings.py
+++ good_world_news/settings.py
@@ -94,13 +94,6 @@ WSGI_APPLICATION = 'good_world_news.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
