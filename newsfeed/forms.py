add80d12c69511bb73540a3fc21ee90a616de230
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-08-04 12:53:06 +0000

    Add docstring to forms in newsfeed

--- newsfeed/forms.py
+++ newsfeed/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from .models import Comment
 
 class CommentForm(forms.ModelForm):
+    """
+    A Django ModelForm for creating and editing comments.
+    """
     class Meta:
         model = Comment
         fields = ['content',]
